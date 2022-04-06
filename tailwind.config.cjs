6114df3153201794b962c5a0a075d5b0cabcabd7
Rudiger Wolf <rudiger.wolf@throughputfocus.com>
2022-04-06 15:27:08 +0100

    Tweak some of the settings for Menu and remove Hero component from  index.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,30 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
+    fill: (theme) => ({
+      current: "currentColor",
+      white: theme("colors.white"),
+    }),
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: colors.white,
+      black: colors.black,
+      gray: colors.slate,
+      blue: {
+        50: "#b3c0ff",
+        100: "#8ea1ff",
+        200: "#7b91ff",
+        300: "#6881ff",
+        400: "#5572ff",
+        500: "#4262ff",
+        600: "#3b58e6",
+        700: "#2e45b3",
+        800: "#213180",
+        900: "#141d4c",
+      },
+    },
     extend: {},
   },
   plugins: [],
