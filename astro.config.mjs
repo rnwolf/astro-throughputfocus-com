987b9d91f659bee950766dc93bfe80383c49f47b
Rudiger Wolf <rudiger.wolf@throughputfocus.com>
2022-04-06 10:08:51 +0100

    Add astro-icon and update nav config via array

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
   integrations: [
     tailwind({
       config: {
