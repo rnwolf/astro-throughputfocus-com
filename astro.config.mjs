86be0c075edb2f4723d77a6737b8bc85dcf2490a
Rudiger Wolf <rudiger.wolf@throughputfocus.com>
2022-04-08 16:46:29 +0100

    Add SiteBanner component in order to announce messages at the top across whole site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import astroImagePlugin from "astro-imagetools/plugin";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    plugins: [astroImagePlugin],
     ssr: {
       external: ["svgo"],
     },
