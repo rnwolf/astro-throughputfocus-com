68803b1c5a78641167a60f4003ecca22d51f6c9b
Rudiger Wolf <rudiger.wolf@throughputfocus.com>
2022-04-12 21:23:48 +0100

    Add site config file and example of how to pass data to component

---
+++ src/website.config.cjs
@@ -0,0 +1,22 @@
+export default {
+  siteTitle: "Throughput Focus Ltd Website",
+  siteDescription:
+    "Website for people that want to improve the performance of their organisation.",
+  favicon: "/favicon.ico",
+  dateFormat: "yyyy-MM-dd HH:mm",
+  socialMedia: {
+    twitter: "throughputfocus",
+    linkedin: "throughputfocus",
+  },
+  pageSize: 5,
+  parentdataarray: [
+    {
+      text: "Home data",
+      name: "John Config",
+    },
+    {
+      text: "About Config",
+      name: "Fred Config",
+    },
+  ],
+};
