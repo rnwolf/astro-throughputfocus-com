8c5ce0a678cb3c74af6067ee488f06e5f63bf964
Rudiger Wolf <rudiger.wolf@throughputfocus.com>
2022-04-23 21:43:14 +0100

    Refactor componets to use data from config file for content.

--- src/website.config.cjs
+++ src/website.config.cjs
@@ -45,4 +45,136 @@ export default {
       link: "/",
     },
   ],
+  selectedPosts: [
+    {
+      heading: "This is the post heading A",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      imageThumbnail:
+        "https://tailus.io/sources/blocks/twocards/preview/images/woman.jpg",
+      imageDescription: "A picture of a woman",
+      link: "/",
+    },
+    {
+      heading: "This is another post heading B",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      imageThumbnail:
+        "https://tailus.io/sources/blocks/twocards/preview/images/man.jpg",
+      imageDescription: "A picture of a man",
+      link: "/",
+    },
+    {
+      heading: "This is the post heading C",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      imageThumbnail:
+        "https://tailus.io/sources/blocks/twocards/preview/images/woman.jpg",
+      imageDescription: "A picture of a woman",
+      link: "/",
+    },
+    {
+      heading: "This is another post heading D",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      imageThumbnail:
+        "https://tailus.io/sources/blocks/twocards/preview/images/man.jpg",
+      imageDescription: "A picture of a man",
+      link: "/",
+    },
+  ],
+  valueStackButtons: [
+    {
+      text: "Cheaper",
+      icon: "mdi:cash-multiple",
+    },
+    {
+      text: "Faster",
+      icon: "mdi:alarm",
+    },
+    {
+      text: "Better",
+      icon: "mdi:arrow-top-right-thick",
+    },
+    {
+      text: "Brilliant",
+      icon: "mdi:account-check",
+    },
+  ],
+  // Is this comment ok here //
+  featuresThreeColumn: [
+    {
+      header: "Header One here 1",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:cash-multiple",
+    },
+    {
+      header: "Header Two here 2",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:alarm",
+    },
+    {
+      header: "Finally header three 3",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:arrow-top-right-thick",
+    },
+  ],
+  // FeatureForRole data
+  roleFeatures: [
+    {
+      description: "Manager",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:cash-multiple",
+      link: "/",
+    },
+    {
+      description: "Software Developer",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:alarm",
+      link: "/",
+    },
+    {
+      description: "Finance",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:arrow-top-right-thick",
+      link: "/",
+    },
+  ],
+  // Stakes TextLeft
+  stakesForTextLeft: [
+    {
+      heading: "Competitive exchange rates A",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:cash-multiple",
+    },
+    {
+      heading: "Faster 1 ",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:alarm",
+    },
+    {
+      heading: "Better 2",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:arrow-top-right-thick",
+    },
+  ],
+  // Stakes TextRight
+  stakesForTextRight: [
+    {
+      heading: "A Competitive exchange rates",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:cash-multiple",
+    },
+    {
+      heading: "Faster",
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      icon: "mdi:alarm",
+    },
+  ],
+  // Picture Text Right
+
+  paragraphs: [
+    {
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!",
+    },
+    {
+      text: "Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.",
+    },
+  ],
 };
