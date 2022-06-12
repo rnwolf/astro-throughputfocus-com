a546f1f77c2c706e2d48c2bcf025a8a16420d7b7
Rudiger Wolf <rudiger.wolf@throughputfocus.com>
2022-06-12 20:44:18 +0100

    Comment out the Honeycomb code

---
+++ functions/_middleware.js
@@ -0,0 +1,6 @@
+// import honeycombPlugin from "@cloudflare/pages-plugin-honeycomb";
+
+// export const onRequest = honeycombPlugin({
+//   apiKey: "${HONEYCOMB_API_KEY}",
+//   dataset: "YOUR_HONEYCOMB_DATASET_NAME",
+// });
