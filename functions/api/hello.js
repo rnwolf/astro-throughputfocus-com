d1c1251b7a3d322e668e0c7be11c8b9a94f2eded
Rudiger Wolf <rudiger.wolf@throughputfocus.com>
2022-06-12 16:26:34 +0100

    Page function move _middleware.js and remove ctx from hello.js

--- functions/api/hello.js
+++ functions/api/hello.js
@@ -4,7 +4,7 @@
  * //functions/api/hello.js
  **/
 
-export async function onRequestPost(request, env, ctx) {
+export async function onRequestPost(request, env) {
   // ...
   const somedata = env.API_KEY_DUMMY;
   return new Response(`Hello world ${somedata}`);
