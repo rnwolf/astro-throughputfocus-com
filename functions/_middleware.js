9fdb08079231a515b85c860401c7d40dbbb27194
Rudiger Wolf <rudiger.wolf@throughputfocus.com>
2022-06-19 16:29:44 +0100

    Update middleware function. did not pass on to pages after running function

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -7,14 +7,37 @@
 
 //https://developers.cloudflare.com/pages/how-to/use-worker-for-ab-testing-in-pages/
 const cookieName = "ab-test-cookie";
-const newHomepagePathName = "/test";
+const newHomepagePathName = "/new-basic";
 
 const abTest = async ({ request, next, env }) => {
-  /*
-  Todo: 
-  1. Conditional statements to check for the cookie
-  2. Assign cookie based on percentage then serve 
-  */
+  const url = new URL(request.url);
+  // if homepage
+  if (url.pathname === "/") {
+    // if cookie ab-test-cookie=new then change the request to go to /test
+    // if no cookie set, pass x% of traffic and set a cookie value to "current" or "new"
+
+    let cookie = request.headers.get("cookie");
+    // is cookie set?
+    if (cookie && cookie.includes(`${cookieName}=new`)) {
+      // pass the request to /test
+      url.pathname = newHomepagePathName;
+      return env.ASSETS.fetch(url);
+    } else {
+      const percentage = Math.floor(Math.random() * 100);
+      let version = "current"; // default version
+      // change pathname and version name for 50% of traffic
+      if (percentage < 50) {
+        url.pathname = newHomepagePathName;
+        version = "new";
+      }
+      // get the static file from ASSETS, and attach a cookie
+      const asset = await env.ASSETS.fetch(url);
+      let response = new Response(asset.body, asset);
+      response.headers.append("Set-Cookie", `${cookieName}=${version}; path=/`);
+      return response;
+    }
+  }
+  return next();
 };
 
 export const onRequest = [abTest];
