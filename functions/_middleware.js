d7db196e6a1a876fe2d8da6223807e6ab92effc9
Rudiger Wolf <rudiger.wolf@throughputfocus.com>
2022-06-19 16:20:59 +0100

    Update middleware.js function and add component to list blog post authors

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -4,3 +4,17 @@
 //   apiKey: "${HONEYCOMB_API_KEY}",
 //   dataset: "YOUR_HONEYCOMB_DATASET_NAME",
 // });
+
+//https://developers.cloudflare.com/pages/how-to/use-worker-for-ab-testing-in-pages/
+const cookieName = "ab-test-cookie";
+const newHomepagePathName = "/test";
+
+const abTest = async ({ request, next, env }) => {
+  /*
+  Todo: 
+  1. Conditional statements to check for the cookie
+  2. Assign cookie based on percentage then serve 
+  */
+};
+
+export const onRequest = [abTest];
