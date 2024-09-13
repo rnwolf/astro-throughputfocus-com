250848aeda9b2e841c13fe020edc95dfbdc8ed0c
Rudiger Wolf <Rudiger.Wolf@ThroughputFocus.com>
2024-09-13 13:20:05 +0100

    playwrite not working remove test blog

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Install the system dependencies for use with playwrite
-apt-get update && apt-get install -y chromium-browser
+#apt-get update && apt-get install -y chromium-browser
 # Intall node packages
 npm install
 # Install Playwright
 npm install -D @playwright/test@latest
 # Install Playwright Browsers
 # --with-deps does not work on cloudflare as no sudo?
+# npx playwright install --with-depts
 npx playwright install chromium
 # name: Build Astro Site
 npm run build -mode production
