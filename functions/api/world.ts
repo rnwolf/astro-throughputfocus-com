f2d79714512bc4796997412a7de55fd026ec3659
Rudiger Wolf <Rudiger.Wolf@ThroughputFocus.com>
2024-09-13 10:35:46 +0100

    Remove old website

--- functions/api/world.ts
+++
@@ -1,8 +0,0 @@
-export const onRequestGet: PagesFunction = async (context) => {
-  //eslint-disable-line
-  const originUrl = new URL(context.request.url).origin;
-  return new Response("<html><body><p>hello world</p></body><html>", {
-    headers: { "content-type": "text/html; charset=UTF-8" },
-    status: 200,
-  });
-};
