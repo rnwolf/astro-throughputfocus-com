636ac5d061b5c333a829a8f95b58b5f4fa1a14a0
Rudiger Wolf <Rudiger.Wolf@ThroughputFocus.com>
2024-09-22 11:36:32 +0100

    Modify cookie expiry from session to 1 day

--- public/_worker.js
+++ public/_worker.js
@@ -11,8 +11,21 @@ export default {
     if (url.pathname === ('/api/fred')) {
       // TODO: Add custom logic here.
       url.pathname = newHomepagePathName;
-      return env.ASSETS.fetch(url);
-      //return new Response('Ok api called');
+
+      const percentage = Math.floor(Math.random() * 100);
+      let version = "current"; // default version
+      // change pathname and version name for 50% of traffic
+      if (percentage < 50) {
+        url.pathname = newHomepagePathName;
+        version = "new";
+      }
+      // get the static file from ASSETS, and attach a cookie
+      const asset = await env.ASSETS.fetch(url);
+      let response = new Response(asset.body, asset);
+      let expires = (new Date(Date.now()+ 86400*1000)).toUTCString(); // Valid for 1 Day = 24 Hrs = 24*60*60 = 86400.
+      response.headers.append("Set-Cookie", `${cookieName}=${version}; expires= ${expires} ;path=/`);
+      return response;
+
     }
     // Otherwise, serve the static assets.
     // Without this, the Worker will error and no assets will be served.
